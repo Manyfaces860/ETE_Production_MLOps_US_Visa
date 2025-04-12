500231e24812e0048caf9c447456bf22dcde5104
Manyfaces860 <127599665+Manyfaces860@users.noreply.github.com>
2025-04-12 17:54:44 +0530

    template creation done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="1.0.0",
+    author="Abhishek",
+    author_email="agblion9@gmail.com",
+    packages=find_packages(),
+    
+)
